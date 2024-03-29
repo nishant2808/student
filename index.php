00eb18c5d4decf47df2dbc3b2076cd92e0261b4b
navneet <nk6914433@gmail.com>
2024-03-29 13:34:24 -0700

    css and form changes

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="style.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<div>
+        <form method="POST">
+            <div class="container">
+                <h1>login Form</h1>
+                
 </head>
-<body>
-	<h1>hello world </h1>
+    <body style="background-color:pink">
+	<!-- <h1>hello world </h1>
 	<h2>hello world </h2>
 	<h3>hello world </h3>
 	<h4>hello world </h4>
 	<h5>hello world </h5>
-	<h6>hello world </h6>
-
+	<h6>hello world </h6> -->
+	
+	<form>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">login</button>
+</form>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
