50b4d8aea4700b24cc307b529338f62fc3a4aa08
navneet <nk6914433@gmail.com>
2024-03-29 13:47:23 -0700

    css and form changes

--- index.php
+++ index.php
@@ -6,35 +6,29 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-	<div>
+</head>
+     <body style="background-color:pink">
+<div>
         <form method="POST">
             <div class="container">
-                <h1>login Form</h1>
+                <h1>Login form</h1>
+				<hr>
                 
-</head>
-    <body style="background-color:pink">
+                <div class="form-group">
+                <input type="text" name="email" placeholder=" Email" class="form-control" required="required"><br>
+                 
+                <input type="text" name="password" placeholder=" Password" class="form-control" required="required"><br>
+                <div class="form-group">
+                <button type="button" name="submit" class="btn btn-primary">Login</button>
+                
 	<!-- <h1>hello world </h1>
 	<h2>hello world </h2>
 	<h3>hello world </h3>
 	<h4>hello world </h4>
 	<h5>hello world </h5>
 	<h6>hello world </h6> -->
-	
-	<form>
-  <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3">
-    </div>
-  </div>
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3">
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">login</button>
-</form>
+	 
+
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
